0608ab5cb8619f3ade5acae33e553e352dc3cfc8
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2025-01-21 00:38:24 +0000

    fix(go-mod-cache): update install script to work in all test scenarios

--- src/go-mod-cache/install.sh
+++ src/go-mod-cache/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 set -e
 
 echo "Activating feature 'go-mod-cache'"
@@ -6,39 +7,38 @@ echo "Activating feature 'go-mod-cache'"
 USERNAME=${USERNAME:-${_REMOTE_USER}}
 GOPATH=${GOPATH:-/go}
 
-create_cache_dir() {
-    local cache_dir=$1
-    local username=$2
-
-    if [ -d "$cache_dir" ]; then
-        echo "Cache directory $cache_dir already exists. Skip creation..."
-    else
-        echo "Create cache directory $cache_dir..."
-        mkdir -p "$cache_dir"
-    fi
-
+setup_groups() {
     if ! getent group golang > /dev/null; then
         echo "Group 'golang' does not exist. Creating..."
         groupadd golang
     else
         echo "Group 'golang' already exists. Skip creation..."
     fi
+}
+
+create_dir() {
+    local target_dir=$1
+    local username=$2
+
+    if [ -d "$target_dir" ]; then
+        echo "Directory $target_dir already exists. Skip creation..."
+    else
+        echo "Create directory $target_dir..."
+        mkdir -p "$target_dir"
+    fi
 
     if [ -z "$username" ]; then
         echo "No username provided. Skip chown..."
     else
-        echo "Change owner of $cache_dir to $username..."
-        chown -R "$username:golang" "$cache_dir"
+        echo "Change owner of $target_dir to $username..."
+        chown -R "$username:golang" "$target_dir"
     fi
 }
 
-create_symlink_dir() {
+symlink_dirs() {
     local local_dir=$1
     local cache_dir=$2
     local username=$3
-
-    runuser -u "$username" -- mkdir -p "$(dirname "$local_dir")"
-    runuser -u "$username" -- mkdir -p "$cache_dir"
 
     # if the folder we want to symlink already exists, the ln -s command will create a folder inside the existing folder
     if [ -e "$local_dir" ]; then
@@ -50,7 +50,9 @@ create_symlink_dir() {
     runuser -u "$username" -- ln -s "$cache_dir" "$local_dir"
 }
 
-create_cache_dir "/mnt/go-cache" "${USERNAME}"
-create_symlink_dir "$GOPATH/pkg" "/mnt/go-cache" "${USERNAME}"
+setup_groups
+create_dir "/mnt/go-cache" "${USERNAME}"
+create_dir "$GOPATH" "${USERNAME}"
+symlink_dirs "$GOPATH/pkg" "/mnt/go-cache" "${USERNAME}"
 
 echo "Finished installing 'go-mod-cache'"
